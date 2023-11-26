eb5340f836327d67d0f8c0fdab7be373cbab464d
Avignon1309 <117497631+Avignon1309@users.noreply.github.com>
2023-11-26 13:39:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -41,3 +41,89 @@ function inititialise() {
 // Inits & Event Listeners
 //
 inititialise();
+
+
+function showBox() {
+  
+  var box = document.getElementById('hiddenBox');
+  box.style.display = 'block';
+
+ 
+  var button = document.getElementById('triggerButton');
+  button.parentNode.removeChild(button);
+}
+
+
+
+
+
+
+
+
+
+let itemArray = [];
+let itemForm = document.getElementById("itemforma");
+let itemList = document.getElementById("itemresults");
+let itemInput = document.getElementById("textInput");
+
+itemForm.addEventListener("submit", handleSubmitForm);
+
+function handleSubmitForm(event) {
+
+
+  event.preventDefault();
+
+ 
+  itemArray.push(itemInput.value);
+
+
+  itemForm.reset();
+
+  renderData();
+}
+
+function renderData() {
+
+  console.log("Render Data", itemArray);
+
+
+  itemList.innerHTML = "";
+
+  for (let i=0; i < itemArray.length ; i++ ){
+    let tempListItem = document.createElement("li");
+
+    tempListItem.textContent = itemArray[i];
+
+    let tempButton = document.createElement("button");
+
+    tempButton.textContent = "FINISHED";
+
+    tempButton.dataset.super = i;
+
+    tempButton.classList.add("finished-button");
+
+    tempButton.addEventListener("click", function(event){
+      console.log("You clicked me");
+
+      console.log("You clicked on", event.target.dataset.super);
+
+ 
+      itemArray.splice(event.target.dataset.super, 1);
+
+      renderData();
+
+      console.log(typeCount[i].type + " - " + typeCount[i].count)
+
+    });
+
+    tempListItem.appendChild(tempButton);
+
+    itemList.appendChild(tempListItem);
+
+  }
+}
+
+
+
+
+
